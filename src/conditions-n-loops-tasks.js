7f6c1ac67aa54b038d44e23887f312b19656bda1
JackalTabaqui <sob.selena@gmail.com>
2025-10-07 21:21:01 +0300

    feat: solve isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -95,8 +95,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (!(a + b > c && a + c > b && c + b > a)) {
+    return false;
+  }
+  return (a === b && a !== c) || (b === c && b !== a) || (a === c && a !== b);
 }
 
 /**
